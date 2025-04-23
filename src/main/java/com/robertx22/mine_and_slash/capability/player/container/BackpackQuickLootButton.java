5ea8b598f1c4ea088ef2c06d0f613f12e0f4cc60
TUsama <1716976420@qq.com>
2025-04-23 11:48:42 +0800

    add drop all function

--- src/main/java/com/robertx22/mine_and_slash/capability/player/container/BackpackQuickLootButton.java
+++ src/main/java/com/robertx22/mine_and_slash/capability/player/container/BackpackQuickLootButton.java
@@ -10,6 +10,7 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.ImageButton;
 import net.minecraft.client.gui.components.Tooltip;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.inventory.ContainerScreen;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.inventory.Slot;
@@ -18,10 +19,19 @@ import net.minecraftforge.client.event.ScreenEvent;
 public class BackpackQuickLootButton extends ImageButton {
     static ResourceLocation TEXTURE = SlashRef.id("textures/gui/backpack_button.png");
     public BackpackQuickLootButton(int pX, int pY, ContainerScreen screen) {
-        super(pX + ClientConfigs.getConfig().QUICK_LOOT_BUTTON_X_OFFSET.get(), pY + ClientConfigs.getConfig().QUICK_LOOT_BUTTON_Y_OFFSET.get(), 14, 14, 0, 0, 16, TEXTURE, 16, 32, x -> Packets.sendToServer(new BackPackLootMenuPacket()), Gui.MASTER_BACKPACK_LOOT_BUTTON.locName());
-        this.setTooltip(Tooltip.create(Gui.MASTER_BACKPACK_LOOT_BUTTON.locName()));
+        super(pX + ClientConfigs.getConfig().QUICK_LOOT_BUTTON_X_OFFSET.get(), pY + ClientConfigs.getConfig().QUICK_LOOT_BUTTON_Y_OFFSET.get(), 14, 14, 0, 0, 16, TEXTURE, 16, 32, x -> Packets.sendToServer(new BackPackLootMenuPacket(Screen.hasShiftDown() ? BackPackLootMenuPacket.Mode.DROP : BackPackLootMenuPacket.Mode.LOOT)), Gui.MASTER_BACKPACK_LOOT_BUTTON.locName());
+
     }
-    
+
+    @Override
+    public void renderWidget(GuiGraphics pGuiGraphics, int pMouseX, int pMouseY, float pPartialTick) {
+        super.renderWidget(pGuiGraphics, pMouseX, pMouseY, pPartialTick);
+        if (Screen.hasShiftDown()){
+            this.setTooltip(Tooltip.create(Gui.MASTER_BACKPACK_LOOT_BUTTON_2.locName()));
+        } else {
+            this.setTooltip(Tooltip.create(Gui.MASTER_BACKPACK_LOOT_BUTTON.locName()));
+        }
+    }
 
     public static void addLootButton(ScreenEvent.Init event){
         if (!(event instanceof ScreenEvent.Init.Post)) return;
--- src/main/java/com/robertx22/mine_and_slash/vanilla_mc/packets/backpack/BackPackLootMenuPacket.java
+++ src/main/java/com/robertx22/mine_and_slash/vanilla_mc/packets/backpack/BackPackLootMenuPacket.java
@@ -8,11 +8,27 @@ import com.robertx22.mine_and_slash.uncommon.datasaving.Load;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.ChestMenu;
 import net.minecraft.world.inventory.Slot;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+
+import java.util.List;
 
 public class BackPackLootMenuPacket extends MyPacket<BackPackLootMenuPacket> {
+    Mode mode;
+
+    public BackPackLootMenuPacket() {
+    }
+
+
+    public BackPackLootMenuPacket(Mode mode) {
+        this.mode = mode;
+    }
+
     @Override
     public ResourceLocation getIdentifier() {
         return SlashRef.id("backpack_loot");
@@ -20,30 +36,50 @@ public class BackPackLootMenuPacket extends MyPacket<BackPackLootMenuPacket> {
 
     @Override
     public void loadFromData(FriendlyByteBuf friendlyByteBuf) {
-
+        mode = friendlyByteBuf.readEnum(Mode.class);
     }
 
     @Override
     public void saveToData(FriendlyByteBuf friendlyByteBuf) {
-
+        friendlyByteBuf.writeEnum(mode);
     }
 
     @Override
     public void onReceived(ExilePacketContext exilePacketContext) {
         if (exilePacketContext.getPlayer() instanceof ServerPlayer player) {
             Backpacks backpacks = Load.backpacks(player).getBackpacks();
-            if (player.hasContainerOpen() && player.containerMenu instanceof ChestMenu chestMenu) {
-                for (Slot slot : chestMenu.slots) {
-                    if (slot.container instanceof Inventory) continue;
-                    backpacks.tryAutoPickup(player, slot.getItem(), false);
+
+                if (player.hasContainerOpen() && player.containerMenu instanceof ChestMenu chestMenu) {
+                    for (Slot slot : chestMenu.slots) {
+                        if (slot.container instanceof Inventory) continue;
+                        ItemStack item = slot.getItem();
+                        if (item.is(Items.AIR)) continue;
+                        if (mode == Mode.LOOT){
+                            backpacks.tryAutoPickup(player, item, false);
+                        } else {
+                            turnItemToPickableAndRemove(item, player);
+                        }
+                    }
                 }
-            }
 
         }
+    }
+
+    public static void turnItemToPickableAndRemove(ItemStack itemStacks, Player player){
+        ItemEntity itemEntity = player.spawnAtLocation(itemStacks.copy(), 1.0f);
+        if (itemEntity != null){
+            itemEntity.setNoPickUpDelay();
+        }
+        itemStacks.shrink(itemStacks.getCount());
     }
 
     @Override
     public MyPacket<BackPackLootMenuPacket> newInstance() {
         return new BackPackLootMenuPacket();
+    }
+
+    public enum Mode {
+        LOOT,
+        DROP
     }
 }
