a9c183ffde3a8643cf927b053fc31e841a9e7387
Robert Skalko <treborx555@gmail.com>
2025-07-26 20:50:32 +0200

    6.3.7

--- src/main/java/com/robertx22/mine_and_slash/a_libraries/jei/RunewordRecipes.java
+++ src/main/java/com/robertx22/mine_and_slash/a_libraries/jei/RunewordRecipes.java
@@ -9,16 +9,16 @@ import java.util.List;
 public class RunewordRecipes {
     public static List<RunewordRecipe> Generate(ExileRegistryContainer<RuneWord> runeWordExileRegistryContainer) {
         return runeWordExileRegistryContainer.getList()
-            .stream()
-            .map(x -> {
-                RunewordRecipe recipe = new RunewordRecipe();
-                recipe.id = x.id;
-                recipe.name = x.name;
-                recipe.runes = x.runes;
-                recipe.slots = x.slots;
-                recipe.stats = x.stats;
-                return recipe;
-            })
-            .toList();
+                .stream().filter(x -> !x.isEmpty())
+                .map(x -> {
+                    RunewordRecipe recipe = new RunewordRecipe();
+                    recipe.id = x.id;
+                    recipe.name = x.name;
+                    recipe.runes = x.runes;
+                    recipe.slots = x.slots;
+                    recipe.stats = x.stats;
+                    return recipe;
+                })
+                .toList();
     }
 }
